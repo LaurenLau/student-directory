f94d2ac8851f809d3ad82ac11da62cc3e4e1035c
LaurenLau <harris_lau@hotmail.co.uk>
2019-07-17 12:25:15 +0000

    Methods added to improve code

--- directory.rb
+++ directory.rb
@@ -10,7 +10,24 @@ students = [ "Dr. Hannibal Lecter",
              "Joffrey Baratheon",
              "Norman Bates" ]
 
-puts "The students of Villains Academy"
-puts "----------------"
-students.each { |students| puts students }
-puts "Overall, we have #{students.count} great students"
+
+#Using methods to improve the code
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------------"
+end
+
+def print(names)
+  names.each do |students|
+    puts students
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
